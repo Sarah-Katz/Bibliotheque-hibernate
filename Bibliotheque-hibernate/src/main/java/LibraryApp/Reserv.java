7f0a52838147548a4c49edaf1a43dd0a638a0a5b
Sarah-Katz <Sarahkatz59@gmail.com>
2022-12-11 12:59:53 +0100

    Added Reserv and JPA

---
+++ Bibliotheque-hibernate/src/main/java/LibraryApp/Reserv.java
@@ -0,0 +1,38 @@
+package LibraryApp;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Sarah Katz
+ *
+ */
+@Entity
+@Table(name = "reserv")
+public class Reserv {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idres")
+	private int idres;
+	@Column(name = "debut")
+	private Date debut;
+	@Column(name = "fin")
+	private Date fin;
+	
+	/**
+	 * @param debut
+	 * @param fin
+	 */
+	public Reserv(Date debut, Date fin) {
+		this.debut = debut;
+		this.fin = fin;
+	}
+	
+	
+}
