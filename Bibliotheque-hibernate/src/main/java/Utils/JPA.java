ab5b2c2c2e1a3f8c51ca6a5f17566e8298aaf8e2
Sarah-Katz <Sarahkatz59@gmail.com>
2022-12-16 12:05:46 +0100

    Doc done

--- Bibliotheque-hibernate/src/main/java/Utils/JPA.java
+++ Bibliotheque-hibernate/src/main/java/Utils/JPA.java
@@ -4,11 +4,32 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * The JPA class provides utility methods for working with the Java Persistence
+ * API (JPA). It includes methods for creating and closing an
+ * EntityManagerFactory and an EntityManager. The EntityManagerFactory is used
+ * to create EntityManager objects, which are used to interact with the database
+ * and perform persistence operations such as creating, reading, updating, and
+ * deleting entity objects.
+ * 
+ * @author Sarah Katz
+ *
+ */
 public class JPA {
+	/** The name of the persistence unit. */
 	private static final String PERSISTENCE_UNIT_NAME = "PERSISTENCE";
+	/** The EntityManagerFactory used to create EntityManager objects. */
 	private static EntityManagerFactory factory;
+	/** The EntityManager used to interact with the database. */
 	private static EntityManager em;
 
+	/**
+	 * Returns the EntityManagerFactory used to create EntityManager objects. If the
+	 * factory has not yet been created, it will be created using the persistence
+	 * unit name specified in the PERSISTENCE_UNIT_NAME field.
+	 * 
+	 * @return the EntityManagerFactory
+	 */
 	public static EntityManagerFactory getEntityManagerFactory() {
 		if (factory == null) {
 			factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
@@ -16,12 +37,20 @@ public class JPA {
 		return factory;
 	}
 
+	/** Closes the EntityManagerFactory if it is not already closed. */
 	public static void shutdownFactory() {
 		if (factory != null) {
 			factory.close();
 		}
 	}
 
+	/**
+	 * Returns the EntityManager used to interact with the database. If the
+	 * EntityManager has not yet been created, it will be created using the
+	 * EntityManagerFactory.
+	 * 
+	 * @return the EntityManager
+	 */
 	public static EntityManager getEntityManager() {
 		if (em == null) {
 			factory = getEntityManagerFactory();
@@ -30,6 +59,7 @@ public class JPA {
 		return em;
 	}
 
+	/** Closes the EntityManager if it is not already closed. */
 	public static void shutdownEntityManager() {
 		if (em != null) {
 			em.close();
